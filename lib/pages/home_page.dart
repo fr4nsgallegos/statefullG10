0067bb69e668b65ff95b8f9a4027c5abd2c50918
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-02 09:45:06 -0500

    build appbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,31 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "SetState Cards Assets App",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              "Selecciona tu mejor elección:",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
