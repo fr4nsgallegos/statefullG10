c8424ceec7e1781af544f1df840c121d240645b4
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-02 10:19:51 -0500

    menu card

---
+++ lib/constants/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+TextStyle tituloStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+TextStyle subTituloStyle = TextStyle(fontSize: 18);
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:statefullg10/constants/constants.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -29,6 +30,7 @@ class HomePage extends StatelessWidget {
               height: 16,
             ),
             Container(
+              padding: EdgeInsets.all(8),
               decoration: BoxDecoration(
                 // color: Colors.red,
                 borderRadius: BorderRadius.circular(20),
@@ -36,19 +38,35 @@ class HomePage extends StatelessWidget {
               ),
               child: Row(
                 children: [
-                  Image.network(
-                    "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                  Container(
                     width: MediaQuery.of(context).size.width / 3,
+                    height: 120,
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
+                  // Image.network(
+                  //   "https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                  //   width: MediaQuery.of(context).size.width / 3,
+                  // ),
                   SizedBox(
                     width: 16,
                   ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text("Menú 1"),
-                      Text("Lun-mar-mier"),
-                      Text("S/ 5")
+                      Text(
+                        "Menú 1",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text("Lun-mar-mier", style: subTituloStyle),
+                      Text("S/ 5", style: tituloStyle)
                     ],
                   )
                 ],
